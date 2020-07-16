d124b941097e0764d5e21b5b88dbcaadd02c3efc
Alvis HT Tang <alvis@hilbert.space>
2020-07-16 18:58:47 +0100

    test: add example files for testing

---
+++ specs/supporting/plain.js
@@ -0,0 +1,6 @@
+module.exports = {
+  a: {
+    b: ['c', 0, true, false, null]
+  },
+  file: 'plain'
+};
---
+++ specs/supporting/plain.ts
@@ -0,0 +1,5 @@
+export const a = {
+  b: ['c', 0, true, false, null],
+};
+
+export const file = 'plain';
